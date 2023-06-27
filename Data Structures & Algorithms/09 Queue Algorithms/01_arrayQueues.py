d27c5fe19803ebd8750be51e6a9be8c40cde6311
thisisaayush <thakurikarthik@gmail.com>
2023-06-26 21:01:32 -0500

    - added queues algorithms.

---
+++ Data Structures & Algorithms/09 Queue Algorithms/01_arrayQueues.py
@@ -0,0 +1,27 @@
+class QueuesArray:
+    def __init__(self):
+        self._data = []
+    
+    def __len__(self):
+        return len(self._data)
+    
+    def isEmpty(self):
+        return len(self._data) == 0
+    
+    def enqueue(self, e):
+        self._data.append(e)
+
+    def dequeue(self):
+        if self.isEmpty():
+            print("Queue is empty!")
+            return 
+        
+        else: 
+            return self._data.pop(0)
+        
+    def firs(self):
+        if self.isEmpty():
+            print("Queue is empty!")
+            return
+
+        return self._data[0]
---
+++ Data Structures & Algorithms/09 Queue Algorithms/02_linkedlistQueues.py
@@ -0,0 +1,61 @@
+class _Node:
+    __slots__ = '_element', '_next'
+
+    def __init__(self, element, next):
+        self._element = element
+        self._next = next
+    
+class QueueLinkedlist:
+
+    def __init__(self):
+        self._front = None
+        self._rear = None
+        self._size = 0
+    
+    def __len__(self):
+        return self._size
+
+    def isEmpty(self):
+        return self._size == 0
+    
+    def first(self, e):
+        new_element = _Node(e, None)
+
+        if self.isEmpty():
+            self._front = new_element
+        
+        else:
+            self._rear._next = new_element
+        
+        self._rear = new_element
+        self._size += 1
+
+    def dequeue(self):
+        if self.isEmpty():
+            print("Queue is empty!")
+            return
+        
+        e = self._front._element
+        self._front = self._front._next
+        self._size -= 1
+
+        if self.isEmpty():
+            self._rear = None
+        
+        return e
+
+    def first(self):
+        if self.isEmpty():
+            print("Queue is empt!")
+            return
+        
+        return self._front._element
+    
+    def display(self):
+        p = self._front
+
+        while p:
+            print(p._element, end=" ")
+            p = p._next
+        print()
+    
