2a2099bd81e21b1e491c07ab0d817c629d9842ce
thisisaayush <thakurikarthik@gmail.com>
2023-06-28 22:23:02 -0500

    - started python course design.

---
+++ Python/Python Concepts/python-concepts.py
@@ -0,0 +1,28 @@
+#----------- python comments- tells the purpose of the code. --------------------------
+'''
+single line comments starts with a "#" sign. 
+multiple lines comments- string- start with triple single or double quotes.
+
+triple single quote: ''''''
+double quote: """"""
+Note: comments are placed in between the given quotes.
+'''
+
+#----------- python variables- store data values. ---------------------------------------
+x = 5 # here "x" is a variable with a value 5 stored in it. 
+name = 'Tony' # here "name" is a variable with a value 'Tony' stored in it.
+
+#------------ python datatypes- defines what type of data stored in a variable. ---------
+x = 5 # x stores int datatype.
+name = 'Tony' # name stores string datatype.
+
+#------------ python casting- converting one data type to another data type. -------------
+x = 5 # x is int datatype.
+y = str(5) # y is string datatype. 5 is string now not an integer.
+
+#------------ python case-sensitive- python names are case sensitive. ---------------------
+a = 5 
+A = "Thor" # a and A are different.
+
+
+
