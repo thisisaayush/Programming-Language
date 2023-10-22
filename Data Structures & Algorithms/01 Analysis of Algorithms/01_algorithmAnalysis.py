936e3d59856b7aa90860def7a21dfd14d30c8914
thisisaayush <thakurikarthik@gmail.com>
2023-10-22 18:46:50 -0500

    - analysis of algorithms.

---
+++ Data Structures & Algorithms/01 Analysis of Algorithms/01_algorithmAnalysis.py
@@ -0,0 +1,38 @@
+'''
+1. Analysis of Algorithms
+    - Analysis of algorithms is the process of evaluating and understanding the efficiency and performance
+      of algorithms. It involves studying how the execution time and resource usage (typically memory) of
+      an algorithm grow with the size of the input data.
+
+    - The primary goals of algorithm analysis are to:
+        a. Compare Algorithms: Compare different algorithms for solving the same problem and determine which one
+        is more efficient in terms of time and space.
+
+        b. Predict Performance: Estimate how long an algorithm will take to run or how much memory it will use for
+        different input sizes.
+
+        c. Identify Bottlenecks: Identify the parts of an algorithm that consume the most resources, helping developers
+        optimize those areas.
+
+    - There are two main aspects to algorithm analysis:
+        a. Time Complexity Analysis: Time complexity measures how the running time of an algorithm grows with the size
+           of the input. It is typically expressed using Big O notation, which provides an upper bound on the growth
+           rate of the algorithm's running time. The most common notations used are O(1), O(log n), O(n), O(n log n),
+           O(n^2), O(2^n), etc. Time complexity analysis helps answer questions like, "How does the running time increase
+           as the input size doubles?"
+
+        b. Space Complexity Analysis: Space complexity measures how the memory usage of an algorithm grows with the input
+           size. It is also expressed using Big 0 notation, and it helps answer questions like, "How much memory is
+           required for this algorithm as the input size increases?" Space complexity analysis is essential to ensure that
+           algorithms don't consume excessive memory, especially when dealing with large datasets.
+
+    - The analysis of algorithms involves several techniques:
+        a. Worst-case analysis: This considers the algorithm's performance under the most adverse conditions. It provides
+           an upper bound on the running time and memory usage.
+
+        b. Average-case analysis: This considers the algorithm's performance on an average input. It is often more
+           challenging than worst-case analysis and may require knowledge of probability and statistics.
+
+        c. Best-case analysis: This looks at the algorithm's performance under the most favorable conditions. It provides
+           a lower bound on the performance, although it's rarely used in practice.
+'''
