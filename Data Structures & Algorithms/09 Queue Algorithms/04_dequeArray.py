15c10d6815f805aa4bb42819479fa2f73dda1817
thisisaayush <thakurikarthik@gmail.com>
2023-10-26 19:45:31 -0500

    worked on DeQue concepts

--- Data Structures & Algorithms/09 Queue Algorithms/04_dequeArray.py
+++ Data Structures & Algorithms/09 Queue Algorithms/04_dequeArray.py
@@ -1,3 +1,15 @@
+'''
+DeQue (Double-ended Queue): In DeQue, elements can be added and removed from both ends of an array or a list.
+
+Common use of DeQue:
+    a. Implementing Stacks and Queues: Deques can be used to implement both stacks and queues. Depending on how you use
+       it, you can mimic the behavior of either data structure.
+    b. Sliding Window Problems: Deques are used in solving sliding window problems where you need to maintain a dynamic
+       window of elements in an array efficiently.
+    c. Efficient Insertion and Removal: Deques allow for efficient insertion and removal of elements from both ends,
+       which can be useful in various scenarios.
+'''
+
 class DEQueArray:
     def __init__(self):
         self._data = []
