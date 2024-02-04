b3c5bc8f7552de9279dc2019a28fbbe127e54268
thisisaayush <thakurikarthik@gmail.com>
2024-02-03 19:48:22 -0600

    - dailyTemp code updated

--- CodingPreparation/NeetCode/04Stack/NeetCode/05dailyTemp.py
+++ CodingPreparation/NeetCode/04Stack/NeetCode/05dailyTemp.py
@@ -1,11 +1,20 @@
-def dailyTemp(temp):
-    result = [0] * len(temp)
+def dailyTemperatures(temperatures):
     stack = []
+    result = [0] * len(temperatures)
     
-    for i, t in enumerate(temp):
-        while stack and t > stack[-1][0]:
-            stackT, stackInd = stack.pop()
-            result[stackInd] = (i - stackInd)
-        stack.append([t, i])
+    for i in range(len(temperatures)):
+        while stack and temperatures[i] > temperatures[stack[-1]]:
+            prev_index = stack.pop()
+            result[prev_index] = i - prev_index
+        stack.append(i)
     
     return result
+
+# Example usage:
+temperatures1 = [73, 74, 75, 71, 69, 72, 76, 73]
+
+result1 = dailyTemperatures(temperatures1)
+
+
+print(f"Input: {temperatures1}")
+print(f"Output: {result1}")
